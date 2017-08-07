0cc79bb450333c40f43fc4d0f8907e121283a437
Chengang.Y <chen21945@yeah.net>
2017-08-07 15:24:46 +0800

    thymeleaf模板使用

--- app-web/src/main/java/org/chen/arch/controller/HelloController.java
+++ app-web/src/main/java/org/chen/arch/controller/HelloController.java
@@ -1,14 +1,28 @@
 package org.chen.arch.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloController {
 	
-	@RequestMapping("hello")
+	@RequestMapping("h")
+	@ResponseBody
 	public String hello(){
 		return "Hello World!";
 	}
 
+	/**
+	 * Thymeleaf
+	 * @param map
+	 * @return
+	 */
+	@RequestMapping("/h1")
+	public String hello(ModelMap map){
+		map.addAttribute("data", "test thymeleaf");
+		//返回模板名称，默认目录 src/main/resources/templates
+		return "hello";
+	}
 }
